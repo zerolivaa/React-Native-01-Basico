cc62d59a3a777cabf4dd7a284c8c3315c484ee52
Aluno-Ralfe <aluno.ralfe@gmail.com>
2020-06-23 19:21:46 +0000

    Aula 05 (TextInput e Button)
    Signed-off-by: Aluno-Ralfe <aluno.ralfe@gmail.com>

--- Basico/components/BoasVindas.js
+++ Basico/components/BoasVindas.js
@@ -1,14 +1,73 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 function BoasVindas(props) {
 
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
   const [mensagem, setMensagem] = useState('Seja bem-vindo(a)!');
+  const [mostraMensagem, setMostraMensagem] = useState(false);
+
+  const _apresentarMensagem = () => {
+    if (!nome == '' && !sobrenome == '') {
+      setMostraMensagem(true);
+    } else {
+      Alert.alert(
+        'Preencimento obrigatório',
+        'Informe o nome e o sobrenome!',
+        [
+          { text: 'OK' },
+        ],
+      );
+    }
+  }
+
+  const _limpar = () => {
+    setNome('');
+    setSobrenome('');
+    setMostraMensagem(false);
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Olá {props.saudacao}</Text>
-      <Text style={styles.texto}>{mensagem}</Text>
+
+      <Text style={styles.titulo}>Identifique-se</Text>
+
+      <TextInput
+        style={styles.campo}
+        placeholder="Digite seu nome"
+        onChangeText={nome => setNome(nome)}
+        value={nome}
+      />
+
+      <TextInput
+          style={styles.campo}
+          placeholder="Digite seu sobrenome"
+          onChangeText={sobrenome => setSobrenome(sobrenome)}
+          value={sobrenome}
+      />
+
+      <View style={styles.botaoContainer}>
+        <Button
+          onPress={_apresentarMensagem}
+          title="Ok"
+        />
+      </View>
+
+      {mostraMensagem && (
+        <View>
+          <Text style={styles.textos}>Olá, {nome} {sobrenome}! {props.saudacao}</Text>
+          <Text style={styles.textos}>{mensagem}</Text>
+        </View>
+      )}
+
+      <View style={styles.botaoContainer}>
+        <Button
+          onPress={_limpar}
+          title="Limpar"
+        />
+      </View>
+
     </View>
   )
 }
@@ -20,10 +79,29 @@ const styles = StyleSheet.create({
      padding: 10,
      margin: 10
   },
-  texto: {
-    fontSize: 14,
+  titulo: {
+    fontSize: 16,
     color: '#FFFFFF',
+    fontWeight: 'bold',
     textAlign: "center",
+  },
+  textos: {
+     fontSize: 14,
+     color: '#FFFFFF',
+     textAlign: "center",
+  },
+  campo: {
+    backgroundColor: '#dcedff',
+    fontSize: 14,
+    marginBottom: 5,
+    marginTop: 5,
+    borderRadius: 5,
+    height: 35,
+  },
+  botaoContainer: {
+    marginBottom: 5,
+    marginTop: 5,
+    borderRadius: 5,
   },
 });
 export default BoasVindas;
